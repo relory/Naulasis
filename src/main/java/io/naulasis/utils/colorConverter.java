7dcb2fe5a300ad5cc888c6c348eb663841e2e4d1
EmpireMTR <zio.sabayev@gmail.com>
2025-01-26 21:53:36 +0200

    A little fix

--- src/main/java/io/naulasis/utils/colorConverter.java
+++ src/main/java/io/naulasis/utils/colorConverter.java
@@ -4,9 +4,9 @@ public class colorConverter {
 
     public static int colorToInt(double Red, double Green, double Blue, double opacity) {
         int alpha = (int) Math.round(opacity);
-        int red = (int) Math.round(opacity);
-        int green = (int) Math.round(opacity);
-        int blue = (int) Math.round(opacity);
+        int red = (int) Math.round(Red);
+        int green = (int) Math.round(Green);
+        int blue = (int) Math.round(Blue);
 
         return (alpha << 24) | (blue << 16) | (green << 8) | red;
     }
